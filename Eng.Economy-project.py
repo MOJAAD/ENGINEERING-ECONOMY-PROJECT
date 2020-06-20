ee89dbf315f18775b159b16892c9923438843b78
MOJAAD <mojaad@outlook.com>
2020-06-20 19:59:35 +0430

    DEFINE FUNCTION COMPLETED

--- Eng.Economy-project.py
+++ Eng.Economy-project.py
@@ -9,47 +9,54 @@
 #                                                                                                                   #
 #################################################### MODULES ########################################################
 import sys,time,os,random
-################################################ PROJECT OBJECTS ####################################################
+################################################## CLEAR SCREEN #####################################################
 def cls():
     os.system(['clear','cls'][os.name=='nt'])
 ################################################### DEFINE FCN ######################################################
 def DEFINE(us):
-    project=[[0,0,0,0,0,0]]
+    info=[0,0,0,0,0,0,0,0]
+    project=[info]
+    if int(us)>1:
+        for counter in range(int(us)):
+            project.append(info)
     us=int(us)-1
-    while True:
-        for counter in range(us):
-            cls()
-            print("\t\t\t ______________________________________")
-            print("\t\t\t|           FOR PROJECT {} :            |".format(counter+1))
-            print("\t\t\t|        FEEL FOLLOWING FIELD :        |")
-            project[counter][0]=input("\t\t\t| ENTER FIRST CAST:")
-            project[counter][1]=input("\t\t\t| ENTER THE VALUE OF ABORTION:")    
-            try:
-                project[counter][2]=int(input("\t\t\t| ENTER MARR(in persent):"))
-                if project[counter][2]<=0 or project[counter][2]>=100:
-                    project[counter][2]=int("typeerror")
-                else :
-                    project[counter][2]=project[counter][2]/100
+    #while True:
+    for counter in range(us):
+        cls()
+        print("\t\t\t ______________________________________")
+        print("\t\t\t|           FOR PROJECT {} :            |".format(counter+1))
+        print("\t\t\t|        FEEL FOLLOWING FIELD :        |")
+        project[counter][0]=input("\t\t\t| ENTER FIRST CAST:")
+        project[counter][1]=input("\t\t\t| ENTER THE VALUE OF ABORTION:")    
+        try:
+            project[counter][2]=int(input("\t\t\t| ENTER MARR(in persent):"))
+            if project[counter][2]<=0 or project[counter][2]>=100:
+                project[counter][2]=int("typeerror")
+            else :
+                project[counter][2]=project[counter][2]/100
                 project[counter][3]=int(input("\t\t\t| ENTER TAX RATE(in persent):"))
-                if project[counter][3]<=0 or project[counter][3]>=100:
-                    project[counter][3]=int("typeerror")
-                else:
-                    project[counter][3]=project[counter][3]/100
-                n=int(input("\t\t\t| ENTER LIFETIME(years):"))
-                r=int(input("\t\t\t| ENTER INTEREST RATE(in persent):"))
-                if r<=0 or r>=100:
-                    r=int("typeerror")
-                print("\t\t\t|______________________________________|")
-                print("\t\t\t|    CHOOSE PRIOD OF INTEREST RATE:    |")
-                print("\t\t\t| 1) ANNUAL                            |")
-                print("\t\t\t| 2) SEMIANNUAL                        |")
-                print("\t\t\t| 4) TRIMESTER                         |")
-                print("\t\t\t| 12) MOUNTLY                          |")
-                t=input("\t\t\t|______________________________________|")
-                if int(t==1) or int(t)==2 or int(t)==4 or int(t)==12 :
-                    r=r/100
-                    project[counter][4]=((1+r)**t)-1
-                
+            if project[counter][3]<=0 or project[counter][3]>=100:
+                project[counter][3]=int("typeerror")
+            else:
+                project[counter][3]=project[counter][3]/100
+            project[counter][4]=int(input("\t\t\t| ENTER LIFETIME(years):"))
+            r=int(input("\t\t\t| ENTER INTEREST RATE(in persent):"))
+            if r<=0 or r>=100:
+                r=int("typeerror")
+            print("\t\t\t|______________________________________|")
+            print("\t\t\t|    CHOOSE PRIOD OF INTEREST RATE:    |")
+            print("\t\t\t| 1) ANNUAL                            |")
+            print("\t\t\t| 2) SEMIANNUAL                        |")
+            print("\t\t\t| 4) TRIMESTER                         |")
+            print("\t\t\t| 12) MOUNTLY                          |")
+            t=input("\t\t\t|______________________________________|")
+            if int(t)==1 or int(t)==2 or int(t)==4 or int(t)==12 :
+                r=r/100
+                project[counter][5]=((1+r)**int(t))-1
+            else:
+                project[counter][4]=int('typeerror')
+            project[counter][6]=int(input("\t\t\t| ENTER ANNUAL PAYMENT:"))
+            project[counter][7]=int(input("\t\t\t| ENTER FUTURE VALUE:"))
                 # while True:
                 #     input("\t\t\t| CHOOSE INCMOE/CAST TYPE:(1-constant 2-in a period 3-no one 4-next)")
                 #     if _=='1':
@@ -60,11 +67,11 @@ def DEFINE(us):
                 #         _=int("typeerror")
                 #     if _=='4':
                 #         break    
-            except TypeError:
-                cls()
-                print("\n\n\n\t\t\tPLEASE ENTER CORRECTLY!")
-                counter -=1
-                continue
+        except TypeError:
+            cls()
+            print("\n\n\n\t\t\tPLEASE ENTER CORRECTLY!")
+            counter -=1
+            continue
             
 #####################################################################################################################
 def wait():
